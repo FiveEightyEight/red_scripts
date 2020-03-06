7f2777d9b32015091fcdbc793e68e0219c927c2b
Robert Abreu <rabreu@fliptrx.com>
2020-03-06 18:07:43 -0500

    commands are now functions

--- red.sh
+++ red.sh
@@ -31,44 +31,48 @@ AUND='\e[4m'
 #Combo
 WARN=$ABOL$CBLA$BYEL$ABLI"WARN"$CRESET
 
-if [ $1 = 'resetapp' ] && [[ -z "$2" ]]
-then
-        printf 'Resetting App...'
-        rm -rf node_modules && npm cache clear --force && rm package-lock.json && npm i
-        printf "\n...Reset Complete"
-        exit 0
-elif [ $1 = 'qa-build' ]
-then
-        git log | head -1 | sed -E 's/commit //g'
-        exit 0
-elif [ $1 = 'resetapp' ] && [ $2 = 'c' ]
-then
-        printf " 🚀 $CLBU Executing $ABLI$1$CRESET...\n"
-        echo -ne "Removing Node Modules...\r"
-        rm -rf node_modules
-        echo -ne "Clearing npm cache... \r"
-        npm cache clear --force
-        echo -ne "Removing package-lock... \r"
-        rm package-lock.json
-        echo -ne "Reinstalling node_modules... \r"
-        npm i
-        echo -ne "add and commiting new package-lock... \r"
-        git status
-        git add package-lock.json
-        [[ -z "$3" ]] && printf "\n"$CLRED"no commit message argument, using default message$CRESET\n" && git commit -m 'reset package-lock' || git commit -m "$3"
-        printf "\n 🛩   "$CGRE"Execution Complete$CRESET\n"
-        exit 0       
-else
-        printf 'WHAT???'
-        exit 1
-fi
+resetapp () {
+        if [ $1 = 'resetapp' ] && [[ -z "$2" ]]
+        then
+                printf 'Resetting App...'
+                rm -rf node_modules && npm cache clear --force && rm package-lock.json && npm i
+                printf "\n...Reset Complete"
+                exit 0
+        elif [ $1 = 'qa-build' ]
+        then
+                git log | head -1 | sed -E 's/commit //g'
+                exit 0
+        elif [ $1 = 'resetapp' ] && [ $2 = 'c' ]
+        then
+                printf " 🚀 $CLBU Executing $ABLI$1$CRESET...\n"
+                echo -ne "Removing Node Modules...\r"
+                rm -rf node_modules
+                echo -ne "Clearing npm cache... \r"
+                npm cache clear --force
+                echo -ne "Removing package-lock... \r"
+                rm package-lock.json
+                echo -ne "Reinstalling node_modules... \r"
+                npm i
+                echo -ne "add and commiting new package-lock... \r"
+                git status
+                git add package-lock.json
+                [[ -z "$3" ]] && printf "\n"$CLRED"no commit message argument, using default message$CRESET\n" && git commit -m 'reset package-lock' || git commit -m "$3"
+                printf "\n 🛩   "$CGRE"Execution Complete$CRESET\n"
+                exit 0       
+        else
+                printf 'WHAT???'
+                exit 1
+        fi
+}
 
+graph () {
+        git log --graph
+}
 
 #function cd() {
 #  builtin cd "$@" && ls -lrthG;
 #}
 
-alias gitgraph="git log --graph"
 
 
 
